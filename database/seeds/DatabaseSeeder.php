a59fb5d68a393a248443eec3ff33e6080f833b21
makiron918 <gonzaress918@gmail.com>
2020-06-13 20:33:00 +0900

    update seeder
    シーダーファイルを編集した。

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -10,7 +10,8 @@ class DatabaseSeeder extends Seeder
      * @return void
      */
     public function run()
-    {
-        // $this->call(UserSeeder::class);
-    }
+{
+    $this->call(PostsTableSeeder::class);
+}
+
 }
