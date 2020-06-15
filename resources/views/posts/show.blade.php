577f09784d2a5028d3f37602aabc3390d3b0033e
makiron918 <gonzaress918@gmail.com>
2020-06-15 16:14:57 +0900

    update delete text
    詳細ページに削除ボタンを実装。
    postsコントローラーにページネイションを記述した。

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -14,6 +14,16 @@
                 <a class="btn btn-primary" href="{{ route('posts.edit', ['post' => $post]) }}">
                     編集する
                 </a>
+                <form
+                    style="display: inline-block;"
+                    method="POST"
+                    action="{{ route('posts.destroy', ['post' => $post]) }}"
+                >
+                    @csrf
+                    @method('DELETE')
+
+                    <button class="btn btn-danger">削除する</button>
+                </form>
             </div>
 
             <section>
